2fcfc6d2f068e64c4b8f49a651bb1669c49db608
MizanRbf <mizanrbf@gmail.com>
2025-11-15 23:05:15 +0600

    static done

---
+++ Module-7/src/static.ts
@@ -0,0 +1,21 @@
+class Counter {
+  static count: number = 0;
+
+  static increment() {
+    return (Counter.count = Counter.count + 1);
+  }
+
+  static decrement() {
+    return (Counter.count = Counter.count - 1);
+  }
+}
+
+const counter1 = new Counter();
+const counter2 = new Counter();
+console.log(Counter.increment());
+console.log(Counter.increment());
+
+// console.log(counter1.increment());
+// console.log(counter1.increment());
+// console.log(counter2.increment());
+// console.log(counter2.increment());
