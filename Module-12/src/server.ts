f1aef18f7c26483ac6d7d98a2508c9e2d74e9963
MizanRbf <mizanrbf@gmail.com>
2025-12-01 23:52:29 +0600

    express server start

---
+++ Module-12/src/server.ts
@@ -0,0 +1,22 @@
+import express, { Request, Response } from "express";
+const app = express();
+const port = 5000;
+
+// parser
+app.use(express.json());
+// app.use(express.urlencoded());
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello next level development!");
+});
+app.post("/", (req: Request, res: Response) => {
+  console.log(req.body);
+  res.status(201).json({
+    success: true,
+    message: "Api is working",
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
